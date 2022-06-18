de8b7d6ec4ba1cdf846460d922a7418c455ccbb0
Romenick Tester <romenick.tester@gmail.com>
2022-06-18 11:37:38 +0100

    SERVER: initial setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Server is running...")
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}...`));
