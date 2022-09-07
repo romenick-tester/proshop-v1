76b69c2a41ebf60e106987fae549282c9c04fa50
Romenick <romenick.tester@gmail.com>
2022-09-07 23:02:34 +0100

    CLIENT: install latest react router dom and test routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Routes, Route, useParams } from "react-router-dom";
+
 import { Footer, Header } from "./layouts";
 import { HomePage } from "./pages";
 
@@ -16,9 +18,24 @@ const App = () => {
 const Main = () => {
     return <main className="py-3">
         <Container>
-            <HomePage />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/test1" element={<h1>Hello World!</h1>} />
+                <Route path="/test2/:id" element={<ParamTesting />} />
+            </Routes>
         </Container>
     </main>
+};
+
+const ParamTesting = () => {
+    const { id } = useParams();
+
+    return (
+        <>
+            <h1>Param Testing</h1>
+            <p>result: {id}</p>
+        </>
+    )
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import "./assets/index.css";
@@ -9,4 +10,8 @@ const container = document.getElementById("app");
 
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+    <Router>
+        <App />
+    </Router>
+);
