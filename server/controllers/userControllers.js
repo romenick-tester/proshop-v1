e4788c49f779d3351e3ea791cfe6a78de04da51f
Romenick <romenick.tester@gmail.com>
2022-09-20 22:47:42 +0100

    SERVER: generate jwt token

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,25 +2,30 @@ require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
 
 const userLogin = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
 
-    if (!user) throw new Error("Authentication error!");
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const payload = {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        };
 
-    const passwordMatched = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatched) {
-        throw new Error("Authentication error!");
+        res
+            .status(200)
+            .json(payload);
+    } else {
+        res.status(401)
+        throw new Error("Invalid crendentials!");
     }
 
-    user.password = undefined;
-
-    res
-        .status(200)
-        .json(user);
 });
 
 module.exports = {
